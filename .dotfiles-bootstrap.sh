97b0d867fe7b6e780b12df8a608d7b3cd979611f
BoneGames <44772942+BoneGames@users.noreply.github.com>
2020-11-08 15:22:37 +1100

    Update .dotfiles-bootstrap.sh

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -9,10 +9,12 @@ curl -fsSL git.io/antigen > $HOME/.local/bin/antigen.zsh &
 # Wait for plugin managers to install
 wait
 
-# Clone large dotfiles and those that require post install
+# Clone large dotfiles and those that require post install (Jackson)
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-tmux.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-x11.git &
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-zsh.git &
+
+# Clone large dotfiles and those that require post install (Ben)
+vcsh clone https://bonegames@github.com/bonegames/dotfiles-zsh.git &
 
 # Wait for dotfiles cloning to finish
 wait
@@ -26,7 +28,7 @@ zsh -c $HOME/.tmux/plugins/tpm/bin/install_plugins &
 # Wait for zsh and tmux caching to finish
 wait
 
-# Clone static dotfiles
+# Clone static dotfiles (Jackson)
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-alacritty.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-alsa.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-autorandr.git &
@@ -35,7 +37,6 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-compton.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-crt.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-dnsmasq.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-dunst.git &
-vcsh clone https://bonegames@github.com/bonegames/dotfiles-git.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-gtk.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-i3.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-jobber.git &
@@ -46,9 +47,12 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ncmpcpp.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-pcmanfm.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-pulseaudio.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ranger.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-xdg.git &
+
+# Clone static dotfiles (Ben)
+vcsh clone https://bonegames@github.com/bonegames/dotfiles-git.git &
 vcsh clone https://bonegames@github.com/bonegames/dotfiles-scripts.git &
 vcsh clone https://bonegames@github.com/bonegames/dotfiles-ssh.git &
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-xdg.git &
 
 # Wait for dotfiles cloning to finish
 wait
