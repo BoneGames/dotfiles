1be73dd881f205507c469207e97dd4509b011da7
BoneGames <44772942+BoneGames@users.noreply.github.com>
2021-01-15 16:53:25 +1100

    Update .dotfiles-bootstrap.sh
    all to sabrehagen except git

--- .dotfiles-bootstrap.sh
+++ .dotfiles-bootstrap.sh
@@ -14,7 +14,7 @@ wait
 
 # Clone large dotfiles and those that require post install
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-tmux.git &
-vcsh clone https://bonegames@github.com/bonegames/dotfiles-zsh.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-zsh.git &
 
 # Wait for dotfiles cloning to finish
 wait
@@ -36,9 +36,9 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-code.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-crt.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-dnsmasq.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-dunst.git &
-vcsh clone https://bonegames@github.com/bonegames/dotfiles-git.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-git.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-gtk.git &
-vcsh clone https://bonegames@github.com/bonegames/dotfiles-i3.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-i3.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-jobber.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-mime.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-mpd.git &
@@ -48,7 +48,7 @@ vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-pcmanfm.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-picom.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-pulseaudio.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ranger.git &
-vcsh clone https://bonegames@github.com/bonegames/dotfiles-scripts.git &
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-scripts.git &
 vcsh clone https://bonegames@github.com/bonegames/dotfiles-ssh.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-x11.git &
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-xdg.git &
